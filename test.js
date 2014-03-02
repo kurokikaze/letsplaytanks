a3d0e7631f17985c4bed89a3010ca768bbf15dfd
Sergey Shirokov <kurokikaze@yandex.ru>
2014-03-02 16:30:52 -0500

    Buttons press test

--- test.js
+++ test.js
@@ -3,25 +3,45 @@ var fs = require('fs');
 var http = require('http');
 
 var self = nes.ui;
-fs.readFile('battlecity.nes', {'encoding' : 'binary'}, function(err, data) {
-	if (err) { return err; }
-	// console.log(nes);
-	self.nes.loadRom(data);
-	self.nes.start();
+var loadAndStart = function() {
+	fs.readFile('battlecity.nes', {'encoding' : 'binary'}, function(err, data) {
+		if (err) { return err; }
+		// console.log(nes);
+		self.nes.loadRom(data);
+		self.nes.start();
 
-	console.log('Enable', self.nes.isRunning);
-    self.enable();
-    console.log('Done!');
-    var canvas = self.nes.ui.screen[0];
-    //console.log(self.nes.ui.canvasImageData);
-    console.log(self.nes.frameTime);
+		console.log('Enable', self.nes.isRunning);
+    	self.enable();
+    	console.log('Done!');
+    	var canvas = self.nes.ui.screen[0];
+    	//console.log(self.nes.ui.canvasImageData);
+    	console.log(self.nes.frameTime);
+		
+		startServer(canvas);
+		var pressed = false;
+		setInterval(function(){
+			if (!pressed) {
+			self.nes.keyboard.keyDown({keyCode: 13, preventDefault: function(){}});
+			console.log('Press');
+			} else {
+			
+			self.nes.keyboard.keyUp({keyCode: 13, preventDefault: function(){}});
+			console.log('Release');
+			}
+			pressed = !pressed;
+		}, 1000);
+	});
+}
 
+var startServer = function(canvas) {
     http.createServer(function (req, res) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.end(''
-            + '<meta http-equiv="refresh" content="1;" />'
-            + '<img src="' + canvas.toDataURL() + '" />');
-    }).listen(3000);
-    console.log('Server started on port 3000');
-});
+    
+   	res.writeHead(200, { 'Content-Type': 'text/html' });
+       	res.end(''
+           + '<meta http-equiv="refresh" content="1;" />'
+           + '<img src="' + canvas.toDataURL() + '" />');
+   	}).listen(3000);
+   	console.log('Server started on port 3000');
+}
 
+loadAndStart();
